eafa8b7c7d76cd0a02cb4c2f6c0d14d34592ef76
kstoltzenburg <kstoltzenburg@users.noreply.github.com>
2016-07-04 22:04:11 +0100

    remove all movie irrelevant lines

--- extract_movie_ratings.pl
+++ extract_movie_ratings.pl
@@ -23,9 +23,9 @@ if ($num_args != 1) {
 my $infile = $ARGV[0];
 
 # -- preprocessing
-# all blogposts are written in one long line in the xml file, 
-# beginning of an individual blogpost is marked by <category>
-# split them up by inserting a line-break before <category> element
+# all blogposts are written in one long line in the xml file
+
+# separate blogpost: break <category> elements into a new line
 # http://perldoc.perl.org/functions/open.html
 
 open (my $in, "<", $infile)   or die "Can't read input file: $!";
@@ -36,7 +36,20 @@ while (<$in>) {
     print $out $_;
 }
 
+
+# remove unrelevant blogposts: remove lines not containing ("term='filmfern'")
+
+# TODO: this looks *cough* slightly *cough* overhead
+
+open ($in, "<", "output.txt")   or die "Can't read input file: $!";
+open ($out, ">", "output_2.txt") or die "Can't write new file: $!";
+
+while (<$in>) {
+    print $out $_ if /term='filmfern'/;
+}
+
 close $out;
+
 
 
 # exract information - movie title (bold?), rating ([0-9]/10), year published
