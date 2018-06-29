c12315d43aa8b906cb0e6d0e9f2a34aebe408b41
Karo Stoltzenburg <kstoltzenburg@gmail.com>
2018-06-29 08:32:04 +0100

    find all titles using beautiful soup (wip)

--- extract_movie_ratings.py
+++ extract_movie_ratings.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python2
 
 import re
-# import string
+from bs4 import BeautifulSoup
 
 input_file = 'testdata/blog-small-example.xml'
 output_file = 'output.xml'
-
 
 # might be to heavy on memory for a large file?
 fobj = open(input_file, 'r')
@@ -20,4 +19,23 @@ for eachLine in data:
 
 output.close()
 
-# remove any lines not containing term='filmfern'
+# only keep lines containing term='filmfern'
+# https://stackoverflow.com/questions/4310659/remove-lines-from-a-text-file-which-do-not-contain-a-certain-string-with-python
+with open('output.xml', 'r') as rfp:
+    with open('more_output.xml', 'w') as wfp:
+        for line in rfp:
+            if 'filmfern' in line:
+                wfp.write(line)
+
+
+# Extract the sections of interest from those
+
+fobj = open('more_output.xml', 'r')
+soup = BeautifulSoup(fobj, 'lxml')
+
+# aDict = soup.find_all({'title': True, 'content': True})
+interest = soup.find_all('title')
+
+print(interest)
+
+fobj.close()
