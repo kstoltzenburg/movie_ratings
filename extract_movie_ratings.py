de8add73f19fbd408d7d764d5bbf80d3b1a0c281
Karo Stoltzenburg <kstoltzenburg@gmail.com>
2018-06-30 15:10:20 +0100

    restructure input / output file operations

--- extract_movie_ratings.py
+++ extract_movie_ratings.py
@@ -1,41 +1,52 @@
 #!/usr/bin/env python2
 
 import re
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 
-input_file = 'testdata/blog-small-example.xml'
-output_file = 'output.xml'
+inf = 'testdata/blog-small-example.xml'
+ouf = 'output.xml'
 
-# might be to heavy on memory for a large file?
-fobj = open(input_file, 'r')
-data = fobj.readlines()
+# from input, split lines by category, write to output
+fobj = open(inf, 'r')
+output = open(ouf, 'w')
+
+for eachLine in fobj:
+    # split lines by adding a line ending before each category tag
+    split_line = re.sub('<category', '\n<category', eachLine)
+    output.write(split_line)
 fobj.close()
-
-output = open(output_file, 'a')  # append to this file
-# for each <category add a new line
-for eachLine in data:
-    result = re.sub('<category', '\n<category', eachLine)
-    output.write(result)
-
 output.close()
 
-# only keep lines containing term='filmfern'
-# https://stackoverflow.com/questions/4310659/remove-lines-from-a-text-file-which-do-not-contain-a-certain-string-with-python
-with open('output.xml', 'r') as rfp:
-    with open('more_output.xml', 'w') as wfp:
-        for line in rfp:
-            if 'filmfern' in line:
-                wfp.write(line)
+# Only keep the lines we're interested in ('filmfern')
+output = open(ouf, 'r')
+lines = output.readlines()
+output.close()
+
+output = open(ouf, 'w')
+for line in lines:
+    if 'filmfern' in line:
+        output.write(line)
+output.close()
 
 
 # Extract the sections of interest from those
 
-fobj = open('more_output.xml', 'r')
-soup = BeautifulSoup(fobj, 'lxml')
+# fobj = open('more_output.xml', 'r')
+# soup = BeautifulSoup(fobj, 'lxml')
+
+# print soup.prettify()
+
+# todo - get all the titles as well together with the content?
+# todo - extract only bold, and ratings.
+
+
+# contentTag = soup.content
+# print contentTag.contents
+
 
 # aDict = soup.find_all({'title': True, 'content': True})
-interest = soup.find_all('title')
+# interest = soup.find_all('title')
 
-print(interest)
+# print(interest)
 
-fobj.close()
+# fobj.close()
